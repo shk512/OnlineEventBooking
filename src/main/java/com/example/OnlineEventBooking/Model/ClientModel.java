24300799d7e5464b30db15f624d776ecbaa1b934
shk512 <@shk512>
2023-02-26 14:24:53 +0500

    26-02-2023

--- src/main/java/com/example/OnlineEventBooking/Model/ClientModel.java
+++ src/main/java/com/example/OnlineEventBooking/Model/ClientModel.java
@@ -1,8 +1,12 @@
 package com.example.OnlineEventBooking.Model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="Client")
 public class ClientModel {
     @Id
     @Column
